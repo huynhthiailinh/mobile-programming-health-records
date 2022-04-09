ab1e086b662cf8422f66dd3d6c4e0eddd29fcf3a
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-04-09 14:49:39 +0700

    Layout with fragment

---
+++ app/src/main/java/com/example/healthrecords/view/NewFragment.java
@@ -0,0 +1,27 @@
+package com.example.healthrecords.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.healthrecords.R;
+
+public class NewFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new, container, false);
+    }
+}
